2269e4388e1a724e8b5eb2f7eba75c33139f60c1
Tristan Cotiga <96755578+Bjonk1911@users.noreply.github.com>
2026-02-24 13:54:13 -0500

    Refactor slideshow implementation in index.php

--- index.php
+++ index.php
@@ -5,45 +5,42 @@ echo "<!-- " . $version . " -->\r\n";
 <!DOCTYPE html>
 <html>
 <head>
-  <title>
-      <?php echo $version; ?>
-  </title>
-  <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
-  <style type="text/css">
+  <title><?php echo $version; ?></title>
+  <meta name="robots" content="noindex, nofollow">
+
+  <style>
     html, body {
       margin: 0;
       padding: 0;
+      width: 100%;
       height: 100%;
       overflow: hidden;
       background-color: black;
     }
 
-    iframe {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      border: none;
-      padding-top: 0;
-      box-sizing: border-box;
-      -moz-box-sizing: border-box;
-      -webkit-box-sizing: border-box;
+    #slideshow {
+      width: 100vw;
+      height: 100vh;
+      object-fit: contain; /* keeps aspect ratio, no cropping */
+      display: block;
+      background-color: black;
     }
   </style>
 </head>
+
 <body>
-<iframe src="datetimenow.html" id="firstFrame" style="visibility: hidden;"></iframe>
-<iframe src="datetimenow.html" id="secondFrame" style="visibility: visible;"></iframe>
+
+<img id="slideshow" src="datetimenow.html">
+
 <script type="text/javascript">
   var urls = [];
-  <?php
+<?php
   $slideshow = file("./files/slideshow.txt");
   $outputSize = 0;
   $delay = 14 / 2 * 1000;
 
   for ($i = 0; $i < count($slideshow); $i++) {
-      if (!(substr($slideshow[$i], 0, 1) === "#")) {
+      if (!(substr(trim($slideshow[$i]), 0, 1) === "#")) {
           $enhancedUrl = trim($slideshow[$i]);
           if (!(substr($enhancedUrl, 0, 4) === "http")) {
               $enhancedUrl = "./files/" . $enhancedUrl;
@@ -56,39 +53,22 @@ echo "<!-- " . $version . " -->\r\n";
       }
   }
   echo "var delay = " . $delay . ";\r\n";
-  ?>
+?>
+
   var urlnow = 0;
-  setTimeout("loadIntoFirstFrame()", 1000);
+  var img = document.getElementById("slideshow");
 
-  function loadIntoFirstFrame() {
-    document.getElementById('firstFrame').src = urls[urlnow];
+  function showNext() {
+    img.src = urls[urlnow];
     urlnow++;
     if (urlnow === urls.length) {
       urlnow = 0;
     }
-    setTimeout("switchToFirstFrame()", delay);
+    setTimeout(showNext, delay);
   }
 
-  function switchToFirstFrame() {
-    document.getElementById('firstFrame').style.visibility = 'visible';
-    document.getElementById('secondFrame').style.visibility = 'hidden';
-    setTimeout("loadIntoSecondFrame()", delay);
-  }
-
-  function loadIntoSecondFrame() {
-    document.getElementById('secondFrame').src = urls[urlnow];
-    urlnow++;
-    if (urlnow === urls.length) {
-      urlnow = 0;
-    }
-    setTimeout("switchToSecondFrame()", delay);
-  }
-
-  function switchToSecondFrame() {
-    document.getElementById('firstFrame').style.visibility = 'hidden';
-    document.getElementById('secondFrame').style.visibility = 'visible';
-    setTimeout("loadIntoFirstFrame()", delay);
-  }
+  setTimeout(showNext, 1000);
 </script>
+
 </body>
 </html>
